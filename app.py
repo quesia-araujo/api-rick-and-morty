9faa4327196417206b21e790ab58db5236291991
quesia-araujo <67472987+quesia-araujo@users.noreply.github.com>
2024-02-17 17:21:04 -0400

    add estudos api

---
+++ app.py
@@ -0,0 +1,44 @@
+import urllib.request, json
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def get_list_characters_page():
+
+    url = "https://rickandmortyapi.com/api/character/"
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    dict = json.loads(data)
+
+    return render_template("characters.html", characters=dict["results"])
+
+@app.route("/profile/<id>")
+def get_profile(id):
+
+    url = "https://rickandmortyapi.com/api/character/" + id
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    dict = json.loads(data)
+
+    return render_template("profile.html", profile=dict)
+
+
+@app.route("/lista")
+def get_list_characters():
+
+    url = "https://rickandmortyapi.com/api/character/"
+    response = urllib.request.urlopen(url)
+    characters = response.read()
+    dict = json.loads(characters)
+
+    characters = []
+
+    for character in dict["results"]:
+        character = {
+            "name": character["name"],
+            "status": character["status"],
+        }
+        characters.append(character)
+
+    return {"characters": characters}
